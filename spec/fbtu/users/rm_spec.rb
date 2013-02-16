25eda3b4344bd6fab5cb021a74f48cfe0315facc
Adam Spiers <fbtu@adamspiers.org>
2013-02-16 17:03:53 +0000

    use spec helpers to clean up rm tests

--- spec/fbtu/users/rm_spec.rb
+++ spec/fbtu/users/rm_spec.rb
@@ -2,36 +2,23 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'spec_hel
 
 describe "fbtu users rm" do
   before(:each) do
-    fbtu %w[apps add --name alpha --app-id 123456 --app-secret abcdef]
-    FakeWeb.register_uri(:get,
-      'https://graph.facebook.com/oauth/access_token?client_id=123456&client_secret=abcdef&grant_type=client_credentials',
-      :body => 'access_token=doublesecret')
-
-    user = {
-      "id" => 21055,
-      "access_token" => 9494864868,
-      "login_url" => "https://facebook.example.com/log_this_guy_in?who=thatguy",
-    }
-
-    FakeWeb.register_uri(:get,
-      'https://graph.facebook.com/123456/accounts/test-users?access_token=doublesecret',
-      :body => {:data => [user]}.to_json)
+    @alpha = add_app('alpha')
+    @alice = add_user_to(@alpha)
   end
 
   it "deletes the user" do
-    FakeWeb.register_uri(:delete,
-      "https://graph.facebook.com/21055?access_token=9494864868",
-      :body => "true")
+    delete_url = "https://graph.facebook.com/#{@alice.id}?access_token=#{@alice.access_token}"
+    FakeWeb.register_uri(:delete, delete_url, :body => "true")
 
-    fbtu %w[users rm --app alpha  --user 21055]
+    fbtu %w[users rm --app alpha --user] + [@alice.id]
 
     FakeWeb.should have_requested(:delete,
-      "https://graph.facebook.com/21055?access_token=9494864868")
+      "https://graph.facebook.com/#{@alice.id}?access_token=#{@alice.access_token}")
   end
 
   it "tells you if there was no such user" do
     lambda do
-      fbtu %w[users rm --app alpha  --user bogus], :quiet => true
+      fbtu %w[users rm --app alpha --user bogus], :quiet => true
     end.should raise_error
     @err.should include("Unknown user")
   end
